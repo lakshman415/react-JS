8c2bcedac1640d459d4e6b54042c204e02cb3820
Lakshman <lakshmanlakshman415@gmail.com>
2024-07-01 21:40:09 +0530

    initial commit

---
+++ basics/src/MyComponent.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const MyComponent = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+      <p>This is a basic React component.</p>
+    </div>
+  );
+};
+
+export default MyComponent;
